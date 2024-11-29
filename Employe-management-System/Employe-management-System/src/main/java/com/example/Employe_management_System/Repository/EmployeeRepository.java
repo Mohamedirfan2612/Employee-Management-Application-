22750b650d6c180b535bc9d3948d20b07fe8a0bb
Mohamedirfan2612 <132117843+Mohamedirfan2612@users.noreply.github.com>
2024-11-29 10:57:17 +0530

    Delete Employe-management-System/Employe-management-System directory

--- Employe-management-System/Employe-management-System/src/main/java/com/example/Employe_management_System/Repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Employe_management_System.Repository;
-
-import com.example.Employe_management_System.Entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee,Long> {
-}
